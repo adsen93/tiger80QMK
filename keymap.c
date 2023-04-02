9ee56e8a57375ba289f2a7262922729bf353f6ac
adsen93 <adsen93@gmail.com>
2023-04-02 16:20:08 -0400

    Changed winlock to toggle on/off
    winlock toggle on/off on layer 1 and put scroll lock on layer 2 (fn)

--- keymap.c
+++ keymap.c
@@ -24,42 +24,19 @@ enum layers {
 };
 
 enum custom_keycodes {
-    WINLOCKON = SAFE_RANGE, 
-    WINLOCKOFF
+    WINLOCK = SAFE_RANGE, 
 };
 
-int winLockSentinel = 0; // 0 for WINLOCKON, 1 for WINLOCKOFF
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
- 
     switch (keycode) {
-        case WINLOCKON: //when pressed, turns winlock and the LED on. LED located at default scroll lock location
+        case WINLOCK: // when pressed toggles between winlock on/off. LED turns on/off also (at scroll lock)
             if (record->event.pressed) {
                 // when keycode WINLOCKON is pressed
-                process_magic(QK_MAGIC_GUI_OFF, record);
-                if (winLockSentinel == 0) { //Only changes LED if winlock changes from on to off.
-                    SEND_STRING(SS_TAP(X_SCROLL_LOCK));
-                    winLockSentinel++;
-                }            
-            } else {
-                // when keycode WINLOCKON is released
+                process_magic(QK_MAGIC_TOGGLE_GUI, record);
+                SEND_STRING(SS_TAP(X_SCROLL_LOCK));
             }
-            break;
-        case WINLOCKOFF:    //when pressed, turns winlock and the LED OFF. LED located at default scroll lock location
-            if (record->event.pressed) {
-                // when keycode WINLOCKOFF is pressed          
-                 process_magic(QK_MAGIC_GUI_ON, record);
-                if (winLockSentinel == 1) { // Only changes LED if winlock changes from off to on.
-                    SEND_STRING(SS_TAP(X_SCROLL_LOCK));
-                    winLockSentinel--;
-                }
-
-            } else {
-                // when keycode WINLOCKOFF is released
-            }
-            break;
-
-    }
+    } 
     return true;
 };
 
@@ -69,7 +46,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LAYER0] = LAYOUT_tkl_f13_ansi_tsangan(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE,    KC_PSCR, WINLOCKON, KC_MPLY,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE,    KC_PSCR, WINLOCK, KC_MPLY,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_VOLD,   KC_VOLU,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,    KC_MYCM,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
@@ -78,7 +55,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     ),
 
     [_LAYER1] = LAYOUT_tkl_f13_ansi_tsangan(
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      BL_TOGG, WINLOCKOFF, RGB_TOG,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      BL_TOGG, KC_SCRL, RGB_TOG,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      BL_STEP, RGB_RMOD,  RGB_MOD,
         KC_NO,   RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      BL_BRTG, KC_NO,  KC_NO,
         KC_NO,   RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO,   KC_NO,   KC_NO,   KC_K,    KC_NO,   KC_NO,   KC_NO,            KC_NO,
